292e23c12df00fe245666caecedbe55fea9cc52a
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-04-02 00:51:31 +0530

    Done

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,38 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "api/v1")
 public class UserController {
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "This is my first REST api";
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllUsers();
     }
 
     @PostMapping("/addUser")
-    public String addUser(){
-        return "This is addUser REST api";
+    public UserDTO addUser(@RequestBody UserDTO userDTO){
+        return userService.addUser(userDTO);
     }
+
+    @PutMapping("/updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser (@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+
 }
